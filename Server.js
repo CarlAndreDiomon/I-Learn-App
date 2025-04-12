abe6bab64c824ea91462b924d7de6bfc7c6f2f20
CarlAndreDiomon <dcarlandrerafols@gmail.com>
2025-04-12 16:37:23 +0800

    Update server.js for user routes

--- Server.js
+++ Server.js
@@ -1,8 +1,10 @@
 import { connectDB } from './config/db.js';
 import express from 'express';
+import usersRoutes from './routes/usersRoutes.js';
 import dotenv from 'dotenv';
 // Load environment variables from .env file
 const app = express();
+app.use(express.json());
 
 dotenv.config();
 const port = process.env.PORT;
@@ -10,6 +12,10 @@ const port = process.env.PORT;
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello World!' });
 });
+
+app.use('/api/auth', usersRoutes);
+
+
 
 connectDB().then(() => app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
