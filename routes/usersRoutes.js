e6187433958db41009532546438755c67d51fc0d
CarlAndreDiomon <dcarlandrerafols@gmail.com>
2025-04-12 16:33:54 +0800

    initial setup of user routes

---
+++ routes/usersRoutes.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { registerStudent, loginStudent, registerTeacher, loginTeacher } from '../controllers/authControllers';
+
+const router = express.Router();
+
+// Route to register a student
+router.post('/registerStudent', registerStudent);
+// Route to login a student
+router.post('/loginStudent', loginStudent);
+// Route to register a teacher
+router.post('/registerTeacher', registerTeacher);
+// Route to login a teacher
+router.post('/loginTeacher', loginTeacher);
